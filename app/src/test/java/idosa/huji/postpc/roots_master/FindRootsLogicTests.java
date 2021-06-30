e30315b30f2e01eec0bf53142a79aa216647eb53
Ido Sagiv <ido10796@gmail.com>
2021-06-30 22:14:00 +0300

    add unit tests to findRootsLogic

--- app/src/test/java/idosa/huji/postpc/roots_master/FindRootsLogicTests.java
+++ app/src/test/java/idosa/huji/postpc/roots_master/FindRootsLogicTests.java
@@ -1,64 +1,108 @@
 package idosa.huji.postpc.roots_master;
 
-import android.app.Application;
-import android.content.Context;
-
-import androidx.lifecycle.LifecycleOwner;
-import androidx.test.core.app.ApplicationProvider;
-import androidx.work.Data;
-import androidx.work.ListenableWorker;
-import androidx.work.OneTimeWorkRequest;
-import androidx.work.WorkInfo;
-import androidx.work.WorkManager;
-
-import org.junit.BeforeClass;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.mockito.Mock;
+
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
 
 @RunWith(RobolectricTestRunner.class)
-//@RunWith(JUnit4.class)
 @Config(sdk = 28)
-public class CalculateRootsWorkerTests {
-    private static Context context;
-    private static Executor executor;
+public class FindRootsLogicTests {
+    private static final int DEF_RUNTIME_MS = 10 * 60 * 1000; // 10 min
 
-    @BeforeClass
-    public static void init() {
-        context = ApplicationProvider.getApplicationContext();
-        executor = Executors.newSingleThreadExecutor();
+    private void assertCalculationResult(long num, long expRoot1, long expRoot2) {
+        final long[] roots = {-1, -1};
+        ArrayList<Long> expectedRoots = new ArrayList<Long>() {
+            {
+                add(expRoot1);
+                add(expRoot2);
+            }
+        };
+
+        FindRootsLogic.findRoots(num, 0, DEF_RUNTIME_MS, new FindRootsListener() {
+            @Override
+            public boolean isStopped() {
+                return false;
+            }
+
+            @Override
+            public void onStopped() {
+            }
+
+            @Override
+            public void onProgress(int progress) {
+            }
+
+            @Override
+            public void onResult(long root1, long root2, double calc_time_sec) {
+                roots[0] = root1;
+                roots[1] = root2;
+            }
+
+            @Override
+            public void onFailure(String msg) {
+            }
+
+            @Override
+            public void onTimeout(long current, double calc_time_sec) {
+            }
+        });
+
+        if (expRoot1 != expRoot2) {
+            Assert.assertNotEquals(roots[0], roots[1]);
+        }
+        Assert.assertTrue(expectedRoots.contains(roots[0]));
+        Assert.assertTrue(expectedRoots.contains(roots[1]));
     }
 
     @Test
-    public void when_calculatingRootsWithSuccess_then_resultShouldReturnCorrectly() {
-//        RootCalcItem item = new RootCalcItem(12);
-//        Data inputData = new Data.Builder()
-//                .putString("calc_item_id", item.getId())
-//                .build();
-//
-//        CalculateRootsWorker worker = (CalculateRootsWorker) TestWorkerBuilder.from(context,
-//                CalculateRootsWorker.class, executor)
-//                .setInputData(inputData)
-//                .build();
-//
-//        ListenableWorker.Result result = worker.doWork();
-//        assertEquals(result, ListenableWorker.Result.success());
-////        assertThat(result, is(ListenableWorker.Result.success()));
+    public void when_calculatingRootsOfNum_then_RootShouldBeCorrect() {
+        assertCalculationResult(9, 3, 3);
+    }
 
+    @Test
+    public void when_calculatingRootsOfPrime_then_RootShouldBeOne() {
+        assertCalculationResult(7, 1, 7);
+        assertCalculationResult(999999000001L, 999999000001L, 1);
     }
 
     @Test
     public void when_calculatingRootsForNegNum_then_calculationShouldFail() {
+        final boolean[] isFailed = {false};
+
+        FindRootsLogic.findRoots(-1, 0, DEF_RUNTIME_MS, new FindRootsListener() {
+            @Override
+            public boolean isStopped() {
+                return false;
+            }
+
+            @Override
+            public void onStopped() {
+            }
+
+            @Override
+            public void onProgress(int progress) {
+            }
+
+            @Override
+            public void onResult(long root1, long root2, double calc_time_sec) {
+            }
+
+            @Override
+            public void onFailure(String msg) {
+                isFailed[0] = true;
+            }
+
+            @Override
+            public void onTimeout(long current, double calc_time_sec) {
+            }
+        });
+
+        Assert.assertTrue(isFailed[0]);
     }
 }
